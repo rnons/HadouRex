f9e49369b864f78c3e8aa0083ddf45dce6522a86
rnons <remotenonsense@gmail.com>
2012-12-21 23:05:06 +0800

    split as pure lib

--- Douban/FM.hs
+++ Douban/FM.hs
@@ -1,35 +1,32 @@
 {-# LANGUAGE DeriveDataTypeable #-}
-module Douban.FM  where
+module Douban.FM  
+    (
+      Song(..)
+    , Channel(..)
+    , getPlaylist
+    , hot
+    , trending
+    , search
+    , channelInfo
+    , musicianID  
+    , parseSong
+    , parseChannel
+    ) where
 
 import Prelude hiding (id)
-import Network.URI
+import Control.Monad
+import Data.Char
+
+import Codec.Binary.UTF8.String (encodeString, decodeString)
 import Network.HTTP
-import Network.Browser
-import Network.HTTP.Cookie
 import Text.HTML.TagSoup
 import Text.JSON
 import Text.JSON.Generic
-import System.Cmd
-import System.Exit
-import System.IO
-import System.Directory
-import GHC.IO.Exception
-import Codec.Binary.UTF8.String (encodeString, decodeString)
-import Control.Monad.IO.Class (liftIO)
-import Data.Char
-import Data.Default
-import Data.List
-import Control.Monad
-import Control.Concurrent
-import Control.Exception
-import System.Console.ANSI
-import Douban.State
-import Douban.Search
-import Douban.Utils
 
--- there are two types of json response: song & ad.
--- the 'company', 'rating_avg', 'public_time', 'ssid' fields are missing in ad_json
--- the 'like' and 'length' fields are of String type in ad_json
+
+-- | There are two types of json response: song & ad.
+-- No 'company', 'rating_avg', 'public_time', 'ssid' fields in ad_json.
+-- The 'like' and 'length' fields are of String type in ad_json.
 data Song = Song {
     picture :: String,
     albumtitle :: String,
@@ -48,17 +45,106 @@ data Song = Song {
     aid :: String
 } deriving (Eq, Show, Data, Typeable)
 
-data FavChannel = FavChannel {
-     fav_id    ::  String
-    ,fav_name   ::  String
+
+-- | Channel json response structure.
+data Channel = Channel {
+    intro :: String,
+    name :: String,
+    song_num :: Int,
+    --creator :: Creator,
+    banner :: String,
+    cover :: String,
+    id :: Int,
+    hot_songs :: [String]
 } deriving (Eq, Show, Data, Typeable)
 
--- filter out ads :-)
+
+-- | If success, a list of song will be returned.
+-- Failure not accounted at present.
+getPlaylist :: [(String, String)] -> IO [Song]
+getPlaylist reqData = do
+    let url = "http://douban.fm/j/mine/playlist?" ++ urlEncodeVars reqData
+    rsp <- simpleHTTP $ getRequest url
+    json <- getResponseBody rsp
+    return $ parseSong json
+
+
+-- | Return a list of hot channels.
+hot :: IO [Channel]
+hot = search' url
+  where
+    url = "http://douban.fm/j/explore/hot_channels"
+
+
+-- | Return a list of up trending channels.
+trending :: IO [Channel]
+trending = search' url
+  where
+    url = "http://douban.fm/j/explore/up_trending_channels"
+
+
+-- | Return a list of channels matching provided keywords.
+search :: String -> IO [Channel]
+search [] = return []
+search key = search' url
+  where 
+    url = "http://douban.fm/j/explore/search?query=" ++ 
+          -- encodeString: encode chinese characters
+          urlEncode (encodeString key)
+
+
+search' :: String -> IO [Channel]
+search' url = do
+    rsp <- simpleHTTP $ getRequest url
+    json <- getResponseBody rsp
+    return $ parseChannel json
+
+
+-- | Return the channel matching provided channel id.
+channelInfo :: String -> IO (Maybe Channel)
+channelInfo "0" = return $ Just ch
+  where
+    ch = Channel { intro = "私人", name = "私人", 
+                   song_num = 1001, banner = "", 
+                   cover = "", id = 0, hot_songs = [] }
+channelInfo cid = do
+    let url = "http://douban.fm/j/explore/search?query=" ++ cid
+    rsp <- simpleHTTP $ getRequest url
+    json <- getResponseBody rsp
+    let chs = parseChannel json
+        match = filter (\c -> id c == read cid) chs
+    case match of 
+         [] -> return Nothing
+         cs -> return $ Just $ head cs
+
+
+-- | Return musician id matching provided musician name.
+musicianID :: String -> IO (Maybe String)
+musicianID name = do
+    let url = "http://music.douban.com/search/" ++ urlEncode (encodeString name)
+    rsp <- simpleHTTP $ getRequest url
+    body <- getResponseBody rsp
+    let tags = parseTags body
+        -- how to use `sections` when there are two class names?
+        sec = sections (~== "<div class=answer_left>") tags
+    case sec of
+         [] -> return Nothing
+         _  -> do
+             let musician_tags = (head sec) !! 4
+                 musician_info = (\(TagOpen "a" x) -> x) musician_tags
+                 (_, resp_name) =  musician_info !! 2
+                 musician_name = decodeString resp_name
+                 (_, href) = musician_info !! 1
+             return $ Just $ filter isDigit href
+
+
+-- | Parse json response of getPlaylist to [Song].
+-- Capable of filtering out ads :-)
+parseSong :: String -> [Song]
 parseSong json = do
     let decoded = decode json :: Result (JSObject JSValue)
     let value = decoded >>= valFromObj "song" :: Result [JSObject JSValue]
     let songs = (\(Ok x) -> x) value
-    --let folding = foldr (\x acc -> if 
     let filterSong s = do
         let ssid = valFromObj "ssid" s :: Result JSValue
         case ssid of
@@ -67,264 +153,15 @@ parseSong json = do
     let ss = filter filterSong songs
     decodeJSON $ decodeString $ encode ss :: [Song]
 
-getPlaylist t = do
-    ch_id <- getsST st_ch_id
-    context <- getsST st_context
-    let rdata = [("type", t),
-                 ("channel", ch_id),
-                 ("context", context),
-                 ("from", "HadouRex")
-                ]
-    let url = "http://douban.fm/j/mine/playlist?" ++ urlEncodeVars rdata
-    rsp <- simpleHTTP $ getRequest url
-    json <- getResponseBody rsp
-    let songs = parseSong json
-    handle ((\_ -> onError)::SomeException -> IO ()) (play songs)
 
-play :: [Song] -> IO ()
-play [] = getPlaylist "n"
-play (x:xs) = do
-    setSGR [SetConsoleIntensity BoldIntensity,
-            SetColor Foreground Vivid Green]
-    putStrLn ""
-    putStr $ artist x ++ " - " ++ title x
-    setSGR [Reset]
-    putStrLn ""
-    withST $ \st -> do
-        hin <- readMVar (writeh st)
-        hPutStrLn hin $ "LOAD " ++ url x
-        hFlush hin
-    silentlyModifyST $ \st -> st { st_s_picture     =   picture x,
-                                   st_s_albumtitle  =   albumtitle x,
-                                   st_s_album       =   album x,
-                                   st_s_artist      =   artist x,
-                                   st_s_title       =   title x,
-                                   st_s_sid         =   sid x,
-                                   st_s_ssid        =   ssid x,
-                                   st_s_aid         =   aid x
-                                 }
-    liftIO $ withST $ \st -> do
-        hin <- readMVar (writeh st)
-        hout <- readMVar (readh st)
-        mpg123wait hin hout
-    play xs
-    where 
-        mpg123wait hin hout = do
-            line <- hGetLine hout
-            case line of
-                 "@P 0" -> do
-                     --reportEnd
-                     return ExitSuccess
-                 _ -> mpg123wait hin hout
+-- | Parse json response of hot, trending, search to [Channel].
+parseChannel :: String -> [Channel]
+parseChannel json = do
+    let decoded = decode json :: Result (JSObject JSValue)
+    let value = decoded >>= valFromObj "data" >>= 
+                valFromObj "channels" :: Result [JSObject JSValue]
+    let channels = (\(Ok x) -> x) value
+    decodeJSON $ decodeString $ encode channels :: [Channel]
 
-onError = do
-    putStrLn "!!! Wrong channel_id !!!"
-    selectChannel
 
-select = do
-    pprMarks
-    selectChannel
-
-listen [] = select
-listen (x:xs)
-    | isChId x = listenCid x
-    | otherwise = listenArtist x
-
-listenCid ch_id = do
-    putStr "您正在收听的是: "
-    ch <- channelInfo ch_id 
-    let ch_name = name ch 
-    putStrLn $ name ch ++ " MHz"
-    silentlyModifyST $ \st -> st {  st_ch_id = ch_id
-                                   ,st_ch_name = name ch
-                                   ,st_ch_intro = intro ch
-                                   ,st_ch_picture = banner ch }
-    forkIO $ getPlaylist "n"
-    return ()
-
-listenArtist name = do
-    let url = "http://music.douban.com/search/" ++ urlEncode (encodeString name)
-    rsp <- simpleHTTP $ getRequest url
-    body <- getResponseBody rsp
-    let tags = parseTags body
-        -- how to use `sections` when there are two class names?
-        sec = sections (~== "<div class=answer_left>") tags
-    case sec of
-         [] -> do
-             putStrLn $ "Sorry, douban.fm knows no musician named " ++ name
-             selectChannel
-         _  -> do
-             let musician_tags = (head sec) !! 4
-                 musician_info = (\(TagOpen "a" x) -> x) musician_tags
-                 (_, resp_name) =  musician_info !! 2
-                 musician_name = decodeString resp_name
-                 (_, href) = musician_info !! 1
-                 musician_id = filter isDigit href
-             putStr "您正在收听的是: " 
-             putStrLn $ musician_name ++ " MHz"
-             silentlyModifyST $ \st -> st {  st_ch_id = "0"
-                                            ,st_ch_name = musician_name
-                                            ,st_ch_intro = musician_name
-                                            ,st_context = "channel:0|musician_id:" ++ musician_id
-                                          }
-             forkIO $ getPlaylist "n"
-             return ()
-
-isChId :: [Char] -> Bool
-isChId [] = True
-isChId (x:xs) = isDigit x && isChId xs
-
-selectChannel = do
-    putStrLn "Please enter the channel_id you want to listen to:"
-    inpStr <- getLine
-    -- default channel: "1001294 PostRock Odyssey"
-    let channel_id = if inpStr `elem` ["","-3"] then "1001294" else inpStr
-    listen [channel_id]
-
-channelInfo "0" = do
-    let ch = Channel { intro = "私人", name = "私人", 
-                   song_num = 1001, banner = "",
-                   cover = "", id = 0, hot_songs = [] }
-    return ch
-channelInfo cid = do
-    let rdata = [
-                 ("channel", cid)
-                ]
-    let url = "http://douban.fm/j/explore/search?query=" ++ cid
-    rsp <- simpleHTTP $ getRequest url
-    json <- getResponseBody rsp
-    let chs = parseChannel json
-        tc = filter (\c -> Douban.Search.id c == read cid) chs
-    return $ head tc
-
-login _ = do  
-    putStr "Username: "
-    username <- getLine
-    password <- getPassword
-    let fdata = [("form_email", username), ("form_password", password)]
-    rsp <- simpleHTTP (postRequestWithBody "http://www.douban.com/accounts/login" "application/x-www-form-urlencoded" $ urlEncodeVars fdata)
-    let headers = rspHeaders $ (\(Right r) -> r) rsp
-    print headers
-    let (err, cookies) = processCookieHeaders "" headers
-        dbcl2 = ckValue $ last cookies
-        --bid = ckValue $ (cookies !! 1)
-    headers <- newMVar (cookiesToHeader cookies)
-    --silentlyModifyST $ \st -> st { headers = headers, dbcl2 = dbcl2, bid = bid }
-    print dbcl2
-    --print bid
-    select
-    --return ()
-
-getPassword :: IO String
-getPassword = do
-    putStr "Password: "
-    hFlush stdout
-    pass <- withEcho False getLine
-    putChar '\n'
-    return pass
-
-withEcho :: Bool -> IO a -> IO a
-withEcho echo action = do
-  old <- hGetEcho stdin
-  bracket_ (hSetEcho stdin echo) (hSetEcho stdin old) action
-
-getBookmarkFile = do
-    dir <- getAppUserDataDirectory "dourex"
-    let filepath = dir ++ "/bookmarks.json"
-    exist <- doesFileExist filepath
-    if exist then return filepath
-             else do
-                 createDirectory dir
-                 writeFile filepath ""
-                 return filepath
-
-mark ("current":xs) = do
-    ch_id <- getsST st_ch_id
-    ch_name <- getsST st_ch_name
-    markCore ch_id ch_name
-mark (ch_id:xs) = do
-    ch_name <- fmap name (channelInfo ch_id)
-    markCore ch_id ch_name
-    shutdown
-mark _ = do
-    showHelp []
-    shutdown
-
-markCore ch_id ch_name = do
-    let ch = FavChannel { fav_id = ch_id, fav_name = ch_name }
-    bookmarks <- getBookmarkFile
-    json <- readFile bookmarks
-    case json of
-         "" -> do
-             let newjson = encodeJSON [ch]
-             writeFile bookmarks newjson
-             putStrLn "Marked"
-         _ -> do
-             let fav_ch = decodeJSON json :: [FavChannel]
-             if ch `elem` fav_ch then putStrLn "Already marked!"
-                    else do
-                        let newjson = encodeJSON $ ch:fav_ch
-                        writeFile bookmarks newjson
-                        putStrLn "Marked"
-
-unmark ("current":xs) = do
-    ch_id <- getsST st_ch_id
-    ch_name <- getsST st_ch_name
-    unmarkCore ch_id ch_name
-unmark (ch_id:xs) = do
-    ch_name <- fmap name (channelInfo ch_id)
-    unmarkCore ch_id ch_name
-    shutdown
-unmark _ = do
-    showHelp []
-    shutdown
-
-unmarkCore ch_id ch_name = do
-    let ch = FavChannel { fav_id = ch_id, fav_name = ch_name }
-    bookmarks <- getBookmarkFile
-    json <- readFile bookmarks
-    case json of
-         "" -> do
-             let newjson = encodeJSON ch
-             writeFile bookmarks newjson
-             putStrLn "Not marked!"
-         _ -> do
-             let fav_ch = decodeJSON json :: [FavChannel]
-             if ch `notElem` fav_ch then putStrLn "Not marked!"
-                    else do
-                        let newjson = encodeJSON $ delete ch fav_ch
-                        writeFile bookmarks newjson
-                        putStrLn "Unmarked"
-
-marks _ = do
-    pprMarks
-    shutdown
-
-pprMarks = do
-    bookmarks <- getBookmarkFile
-    json <- readFile bookmarks
-    case json of
-         "" -> do
-             putStrLn "You have no channel in your bookmark."
-             putStrLn "Give a try to `dourex hot` or `dourex trending`."
-         _ -> do
-             putStrLn "Bookmarked channels:"
-             let fav_ch = decodeJSON json :: [FavChannel]
-             forM_ fav_ch (\c -> do
-                setSGR [SetConsoleIntensity BoldIntensity]
-                putStr $ "* " ++ fav_name c 
-                setSGR [SetColor Foreground Vivid Green]
-                putStr $ " cid=" ++ show (fav_id c)
-                setSGR [Reset]
-                putStrLn ""     -- [Reset] won't work without this
-                )
-    
-isMarked ch = do
-    bookmarks <- getBookmarkFile
-    json <- readFile bookmarks
-    case json of
-         [] -> return False
-         _ -> do
-             let fav_ch = decodeJSON json :: [FavChannel]
-             return $ ch `elem` fav_ch
 
--- README.md
+++ README.md
@@ -1,55 +1,15 @@
-# HadouRex
+# doubanfm.hs
 
-HadouRex is a douban.fm cli client written in haskell. I'm rather new to haskell, much of the code are based on or inspired by [hackage: hmp3](http://hackage.haskell.org/package/hmp3). The recommendation mechanism is based on [exaile-doubanfm-plugin](https://github.com/sunng87/exaile-doubanfm-plugin).
+doubanfm.hs is a [douban.fm] wrapper library written in haskell.
+
+It is splited from [HadouRex], which is a CLI client.
 
 ## How-To
 
-### To run
 <pre>
-runhaskell dourex.hs
-</pre>
-
-### To build
-
-<pre>
-ghc --make dourex.hs -threaded
-</pre>
-
-### To install
-
-<pre>
+git clone https://github.com/rnons/doubanfm.hs.git
 cabal install
 </pre>
 
-## Commands
-
-<pre>
-dourex
-dourex listen [cid/artist]
-dourex search keywords
-dourex hot
-dourex trending
-dourex mark cid
-dourex unmark cid
-dourex marks
-</pre>
-
-In `dourex listen arg`, if arg is all integer, dourex will assume it to be *channel id*. Otherwise, it will be assumed as *artist name*.  
-
-Note: You need to quote the *artist name* when it consists of more than one word. e.g.
-
-`dourex listen "sigur ros"`
-
-When playing, press `h` to see a full list of available controls.
-
-## Dependencies
-
-[hackage: HTTP](http://hackage.haskell.org/package/HTTP-4000.2.6)  
-[hackage: json](http://hackage.haskell.org/package/json-0.7)  
-[hackage: ansi-terminal](http://hackage.haskell.org/package/ansi-terminal)  
-[hackage: tagsoup](http://hackage.haskell.org/package/tagsoup)  
-[mpg123](http://www.mpg123.de/)  
-
-## Documentation
-
-[The Making of HadouRex](https://github.com/rnons/HadouRex/wiki/The-Making-of-HadouRex)
+[douban.fm]: http://douban.fm
+[HadouRex]: https://github.com/rnons/HadouRex
